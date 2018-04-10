7c825aab8c14d414525d28d5e01a6344d2e083a9
alis <31563315+alisowicz@users.noreply.github.com>
2018-04-10 23:32:20 +0200

    details

--- script.js
+++ script.js
@@ -11,10 +11,39 @@
 const menu = function() {
    if(window.pageYOffset > 50) {
      document.querySelector('.borba_topbar').classList.add('borba_topbar-scroll');
-
    } else {
      document.querySelector('.borba_topbar').classList.remove('borba_topbar-scroll')
-        }
+    }
  }
 
  document.addEventListener('scroll', () => menu())
+
+
+
+
+   //gładkie przewijanie
+
+   $(document).ready(function() {
+     $('a[href^="#"]').on('click', function(event) {
+       var target = $($(this).attr('href'));
+       if (target.length) {
+         event.preventDefault();
+         $('html, body').animate({
+           scrollTop: target.offset().top
+         }, 1000);
+       }
+     });
+   });
+   //koniec gładkiego przewijania
+
+
+
+
+
+
+
+
+
+if ([])
+{console.log('foo');}
+else {console.log('bar');}
